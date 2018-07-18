82981c17e51ec4acefb5526ac4b43f54c485865f
cjh <1418503647@qq.com>
2018-07-19 02:55:58 +0800

    add CDN

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import http from '@/util/http'
-import qs from 'qs'
 import storage from '@/util/storage'
 import ui from './components/index'
 import './util/highlight'
@@ -10,8 +8,6 @@ import './scss/main.scss'
 
 Vue.config.productionTip = false
 
-Vue.prototype.$http = http
-Vue.prototype.$qs = qs
 Vue.prototype.$storage = storage
 
 Vue.use(ui)
